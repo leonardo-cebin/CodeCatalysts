7c69cc1f692b0267077ba66129c990ca640d16db
Leonardo S. Cebin <pepesilvia21@protonmail.com>
2024-12-04 17:15:20 -0300

    Create tables.sql

---
+++ prototipo/database/tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `pumps` (
+    `ID`	INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `NomeModelo`	VARCHAR(512) NOT NULL,
+    `CodigoModelo` VARCHAR(64),
+    `Fabricante` VARCHAR(64),
+    `LocalOrigem` VARCHAR(32),
+    `PressaoMin`	FLOAT,
+    `PressaoMax`	FLOAT,
+    `TempMin`	FLOAT,
+    `TempMax`	FLOAT,
+    `NPSHmin`	FLOAT,
+    `Head_drop_loss`	FLOAT
+  
+);
